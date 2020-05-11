34dc733be425276b3d25288c059ce21db447e865
Allen Boynton <alboynton4@gmail.com>
2020-05-11 01:57:21 -0400

    Update designs.js
    Changed var to let for consistency.

--- designs.js
+++ designs.js
@@ -3,10 +3,11 @@ const colorPicker = document.querySelector('#colorPicker');
 const sizePicker = document.querySelector('#sizePicker');
 const table = document.querySelector('#pixelCanvas');
 
-// Access submit button and assign event listener when pressed
-// Clear any inner HTML from table
-// Get the input from the user and store in a variable
-// When size is submitted by the user, call makeGrid()
+/* Access submit button and assign event listener when pressed
+*  Clear any inner HTML from table
+*  Get the input from the user and store in a variable
+*  When size is submitted by the user, call makeGrid()
+*/
 sizePicker.addEventListener('submit', function() {
   table.innerHTML = "";
   event.preventDefault();
@@ -15,14 +16,15 @@ sizePicker.addEventListener('submit', function() {
   makeGrid(height, width);
 });
 
-// Create for loop for number of rows
-// Create second loop for number of columns
-// Insert both into the table
-// Add event listener to cell click to add color.value
+/* Create for loop for number of rows
+*  Create second loop for number of columns
+*  Insert both into the table
+*  Add event listener to cell click to add color.value
+*/
 function makeGrid(height, width) {
-  for (var x = 0; x < height; x++) {
+  for (let x = 0; x < height; x++) {
     let row = table.insertRow(x);
-    for (var y = 0; y < width; y++) {
+    for (let y = 0; y < width; y++) {
       let cell = row.insertCell(y);
       cell.addEventListener('click', function() {
         cell.style.backgroundColor = colorPicker.value;
